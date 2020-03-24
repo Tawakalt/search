5747cf289b7d84f96c77bbde0fb0ec871ed912ca
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-03-24 21:13:52 +0100

    make sure param q is present

--- places/views.py
+++ places/views.py
@@ -8,37 +8,43 @@ load_dotenv()
 
 def index(request): 
     q = request.GET.get("q", False)
-    API_KEY = os.getenv('API_KEY')
-    searchUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&"
-    searchResponse = requests.get(f'{searchUrl}input={q}&key={API_KEY}') 
-    placeSearch = searchResponse.json()
+    if q:
+        API_KEY = os.getenv('API_KEY')
+        searchUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&"
+        searchResponse = requests.get(f'{searchUrl}input={q}&key={API_KEY}') 
+        placeSearch = searchResponse.json()
 
-    if placeSearch['candidates']:
-        places = placeSearch['candidates']
-        place_id = places[0]['place_id']
-        detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id="
-        detailsResponse = requests.get(f'{detailsUrl}{place_id}&key={API_KEY}')
-        details = detailsResponse.json()
-        result = details['result']
-        needed = {
-            'ID': result['id'],
-            'Provider': result['scope'],
-            'Name': result['name'],
-            'Location': {
-                'Lat': result['geometry']['location']['lat'],
-                'Lng': result['geometry']['location']['lng'],
-            },
-            'Address': result['formatted_address'],
-            'URI': result['url']
+        if placeSearch['candidates']:
+            places = placeSearch['candidates']
+            place_id = places[0]['place_id']
+            detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id="
+            detailsResponse = requests.get(f'{detailsUrl}{place_id}&key={API_KEY}')
+            details = detailsResponse.json()
+            result = details['result']
+            needed = {
+                'ID': result['id'],
+                'Provider': result['scope'],
+                'Name': result['name'],
+                'Location': {
+                    'Lat': result['geometry']['location']['lat'],
+                    'Lng': result['geometry']['location']['lng'],
+                },
+                'Address': result['formatted_address'],
+                'URI': result['url']
+            }
+            status = 'OK'
+        else:
+            needed = []
+            status = placeSearch['status']
+        
+        data = {
+            'status': status,
+            'result': needed
         }
-        status = 'OK'
     else:
-        needed = []
-        status = placeSearch['status']
-    
-    data = {
-        'status': status,
-        'result': needed
-    }
+        data = {
+            'status': 'INVALID REQUEST',
+            'error': 'Parameter q is required'
+        }
         
     return JsonResponse(data)
