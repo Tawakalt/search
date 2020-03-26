51308d3109ccdb19ed71f03020e1a4ef5e1aedec
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-03-26 17:19:24 +0100

    use mock for tests

---
+++ places/mock.py
@@ -0,0 +1,157 @@
+no_candidate = {
+    "candidates": [],
+    "status": "ZERO_RESULTS"
+}
+
+candidate = {
+    'candidates': [{
+        'place_id': 'ChIJwYCC5iqLOxARy9nDZ6OHntw'
+    }],
+    'status': 'OK'
+}
+
+details = {
+    "html_attributions": [],
+    "result": {
+        "address_components": [
+        {
+        "long_name": "Lagos",
+        "short_name": "Lagos",
+        "types": [
+        "locality",
+        "political"
+        ]
+        },
+        {
+        "long_name": "Lagos",
+        "short_name": "LA",
+        "types": [
+        "administrative_area_level_1",
+        "political"
+        ]
+        },
+        {
+        "long_name": "Nigeria",
+        "short_name": "NG",
+        "types": [
+        "country",
+        "political"
+        ]
+        }
+        ],
+        "adr_address": "<span class='locality'>Lagos</span>, <span class='country-name'>Nigeria</span>",
+        "formatted_address": "Lagos, Nigeria",
+        "geometry": {
+        "location": {
+        "lat": 6.5243793,
+        "lng": 3.3792057
+        },
+        "viewport": {
+        "northeast": {
+        "lat": 6.7027591,
+        "lng": 3.4696459
+        },
+        "southwest": {
+        "lat": 6.393351099999999,
+        "lng": 3.0982732
+        }
+        }
+        },
+        "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+        "id": "f5e45a1cb67f5581896d92388b6402c552e4d5f0",
+        "name": "Lagos",
+        "photos": [
+        {
+        "height": 409,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/112172889106278224788'>raji sodiq kehinde</a>"
+        ],
+        "photo_reference": "CmRaAAAAtHbp_tP_w7gxFneI0VFK5eZSStxYZfjzM4U7HMvGZksJ3QKIxJkCwX0QvVWKujEMt1SFLqLLmxniyX0usEHKJ7vCzcH_0rtO6aOKn0rAWIOlVnIvW1_dkkHu4TD2mabdEhDefLiKIe4ndB1WZD7XntbtGhTK24O0uGJS8xHYrMBKcAGGD4205Q",
+        "width": 612
+        },
+        {
+        "height": 303,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/104595100146276585676'>ABU SAFIYA</a>"
+        ],
+        "photo_reference": "CmRaAAAAG8rdPYKJmaV-cU0js0Y6mEECe2N_0ZqMKbPy3G6GJxvFai4TnNoeij2hd5AJrMwFp832cb4PRjLInl205UgnUhCOzidVursg-bUcWfXxccn5l6WUoA6RtdFMRH4rgN8XEhDdIUYtve4I2WKI7e67QqFQGhQU7A-7muPvrt0i38umUSu-vatUeQ",
+        "width": 485
+        },
+        {
+        "height": 768,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/117475456540922569390'>Abiodun Alexis</a>"
+        ],
+        "photo_reference": "CmRaAAAAmeI87r9uLnXTPwv0uXM0Sa99xvWS1ebQFqOj0pb7LUXdDZbSTkRf2OXiixe0UYKFeJ9wYTJxR6p6qVtY49qrigUef56ir2UTgJPuNrGuFUZpPG4i3EYXjT-T4IbwNRQJEhBhNXcOBkYywfSZUusWvOZLGhRvs8TyNp2kESxw_uQapRwrTpin0g",
+        "width": 1024
+        },
+        {
+        "height": 3000,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/105083051436535847790'>Samson Traveljunkie</a>"
+        ],
+        "photo_reference": "CmRaAAAAWFXPsplnpIStz4e_GIJ1oM7nT8baOLFdIfUD60VnTvoYDFj4OfWrY9BeIzzuB3M3rZxN4T5__8raCvxPMZiVQKXpWhGbn531NYHCcMexEYa2BzzWSQhDfx_OjfJLTf6DEhDdUJoo3giCDMIL2hBPDbYAGhRo48TFx2tKDSJjgxd8yDRyVQAJhg",
+        "width": 4000
+        },
+        {
+        "height": 3456,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/110204022241351220320'>emaquist</a>"
+        ],
+        "photo_reference": "CmRaAAAA51rDRgrXMSC2g3-iGsRTB7frcBVGfztEbNS88jq1Ld6RkjgZFh4nfffuvbOcaEudS9nnM8N-OebSsY2Z4kSobPwCYpIFK8KKpzhl84UBSOyzJtQrZajYdKZA0AgDlTmlEhB-b5607cGKE7nYa7qRmj__GhTx3p37HIpjGpZa3Fl7WnWLmgsokA",
+        "width": 4608
+        },
+        {
+        "height": 2448,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/112120700307822080257'>Chris Onah</a>"
+        ],
+        "photo_reference": "CmRaAAAA9TLBvEFGS2AhJG7wsdldsh3-Hzp2ShmPAD9KDypynZSAL4G6oF_q1yV5AJ5d4aWr4IVs1QGzae7GikdhiuECBaMXOGoQM286RzxpwM-WNKq4ZVLgGr0RdqG_tFb344-FEhCLueABZdfxnzGA-E-Gb6jNGhTALoq_JEPUijR8xX7OuRWlVfvSeg",
+        "width": 3264
+        },
+        {
+        "height": 4032,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/108425334718981917869'>ọpẹ́yẹmí balógun</a>"
+        ],
+        "photo_reference": "CmRaAAAAb3B3MRN9mh4XiPmnWHisM2zeCho3TVpguZLT2IZMLlXL0oTIigQryxI1akQSE4f3931T1Pc9224MP5wmCBirYznvtEpirBlO4DkST3p-Dqfizd2dxQjbD8CPobrEa5qgEhDc-USiNJmH7tkeWLqFfyRCGhRvxzBxWhuoVnuA4qFWlkv43dPhiQ",
+        "width": 3024
+        },
+        {
+        "height": 1960,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/110444501670986653436'>Seun Ogunkunle</a>"
+        ],
+        "photo_reference": "CmRaAAAA5XrlcgrgrlBtGGwb8mngafjthqu3y2ofPsXI_hqvPqIaEwYMrCVzauOeJYmxG0OhCfXf4g9Lsi4yGzG4ml_LTcpkWZ0AMm034fKrjDA1nskWQylB9QJowrB_R9kV4f38EhCH0O3r9-0mRQH2uZJ7ewYmGhT_18LxS8bvW7Thqg1Fhj8k1_XArw",
+        "width": 4032
+        },
+        {
+        "height": 383,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/114271257960874618636'>Seun Mosuro</a>"
+        ],
+        "photo_reference": "CmRaAAAA8Ha1wfB2l5R2UvaA_JeL3It5Oz8xfZ5zBWrZKAi5bR-qR3d9ajdiCVo3o0SN_1SHYUDLKus-NLZzjW0XuG2AXBOw7YyA2w8NuYrvVSY6jwdUhxV_OkkS24MfJ2v3b_e9EhDI8zSVywYCjkqQi6ekS5eCGhQWeWv4mSSsxQusoQy_0nb5OwaHYw",
+        "width": 702
+        },
+        {
+        "height": 3265,
+        "html_attributions": [
+        "<a href='https://maps.google.com/maps/contrib/117475456540922569390'>Abiodun Alexis</a>"
+        ],
+        "photo_reference": "CmRaAAAA2SWn-4z5Edk7LvsMYGewEZaTFGQLEdmyat5Pk0KGKtAP9xYpgUm4J-CNW1BmjEuclDcOb6Lrz1VU00NV6Ujp8M5YmVh-XRyqbnwmVZrQwNre97J2-e0_P6nYZqcjNY3qEhCcE24VrNnRIRiAEyUI8WyRGhSOMoatukQBAL-sAXG-Y2LeqkG-9Q",
+        "width": 4898
+        }
+        ],
+        "place_id": "ChIJwYCC5iqLOxARy9nDZ6OHntw",
+        "reference": "ChIJwYCC5iqLOxARy9nDZ6OHntw",
+        "scope": "GOOGLE",
+        "types": [
+        "locality",
+        "political"
+        ],
+        "url": "https://maps.google.com/?q=Lagos,+Nigeria&ftid=0x103b8b2ae68280c1:0xdc9e87a367c3d9cb",
+        "utc_offset": 60,
+        "vicinity": "Lagos"
+    },
+    "status": "OK"
+}
