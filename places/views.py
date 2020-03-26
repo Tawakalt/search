2cfc280d9846b41526ded12f96fcdce57e7f786c
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-03-26 11:56:16 +0100

    implement early return instead of if-else

--- places/views.py
+++ places/views.py
@@ -5,48 +5,50 @@ from dotenv import load_dotenv
 
 def index(request): 
     q = request.GET.get("q", False)
-    if q:
-        lng = request.GET.get("lng", False)
-        lat = request.GET.get("lat", False)
-        API_KEY = os.getenv('API_KEY')
-        searchUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&"
-        if lng and lat:
-            searchResponse = requests.get(f'{searchUrl}input={q}&locationbias=point:{lng},{lat}&key={API_KEY}') 
-        else:
-            searchResponse = requests.get(f'{searchUrl}input={q}&key={API_KEY}') 
-        placeSearch = searchResponse.json()
+    if not q:
+        data = {
+        'status': 'INVALID REQUEST',
+        'error': 'Parameter q is required'
+        }
+        return JsonResponse(data, status=400) 
+ 
+    lng = request.GET.get("lng", False)
+    lat = request.GET.get("lat", False)
+    API_KEY = os.getenv('API_KEY')
+    searchUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&"
 
-        if placeSearch['candidates']:
-            places = placeSearch['candidates']
-            place_id = places[0]['place_id']
-            detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id="
-            detailsResponse = requests.get(f'{detailsUrl}{place_id}&key={API_KEY}')
-            details = detailsResponse.json()
-            result = details['result']
-            needed = {
-                'ID': result['id'],
-                'Provider': result['scope'],
-                'Name': result['name'],
-                'Location': {
-                    'Lat': result['geometry']['location']['lat'],
-                    'Lng': result['geometry']['location']['lng'],
-                },
-                'Address': result['formatted_address'],
-                'URI': result['url']
-            }
-            status = 'OK'
-        else:
-            needed = []
-            status = placeSearch['status']
-        
+    if lng and lat:
+        searchResponse = requests.get(f'{searchUrl}input={q}&locationbias=point:{lng},{lat}&key={API_KEY}') 
+    else:
+        searchResponse = requests.get(f'{searchUrl}input={q}&key={API_KEY}') 
+    placeSearch = searchResponse.json()
+
+    if placeSearch['candidates']:
+        places = placeSearch['candidates']
+        place_id = places[0]['place_id']
+        detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id="
+        detailsResponse = requests.get(f'{detailsUrl}{place_id}&key={API_KEY}')
+        details = detailsResponse.json()
+        result = details['result']
+        needed = {
+            'ID': result['id'],
+            'Provider': result['scope'],
+            'Name': result['name'],
+            'Location': {
+                'Lat': result['geometry']['location']['lat'],
+                'Lng': result['geometry']['location']['lng'],
+            },
+            'Address': result['formatted_address'],
+            'URI': result['url']
+        }
+        status = 'OK'
         data = {
             'status': status,
             'result': needed
         }
-    else:
-        data = {
-            'status': 'INVALID REQUEST',
-            'error': 'Parameter q is required'
-        }
-        return JsonResponse(data, status=400)    
-    return JsonResponse(data)
+        return JsonResponse(data)
+    data = {
+        'status': placeSearch['status'],
+        'result': []
+    }
+    return JsonResponse(data)  
