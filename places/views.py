26cc7c2064005c97e214176abdca78acd8efaf41
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-03-26 12:56:13 +0100

    make app extendable

--- places/views.py
+++ places/views.py
@@ -1,54 +1,26 @@
 from django.http import JsonResponse
 import requests, json, os
-from dotenv import load_dotenv
+from . import sources
 
 
 def index(request): 
     q = request.GET.get("q", False)
-    if not q:
+    source = request.GET.get("source", False)
+    if not (q and source):
         data = {
         'status': 'INVALID REQUEST',
-        'error': 'Parameter q is required'
+        'error': 'Parameters q and source are required'
         }
         return JsonResponse(data, status=400) 
- 
-    lng = request.GET.get("lng", False)
-    lat = request.GET.get("lat", False)
-    API_KEY = os.getenv('API_KEY')
-    searchUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&"
-
-    if lng and lat:
-        searchResponse = requests.get(f'{searchUrl}input={q}&locationbias=point:{lng},{lat}&key={API_KEY}') 
-    else:
-        searchResponse = requests.get(f'{searchUrl}input={q}&key={API_KEY}') 
-    placeSearch = searchResponse.json()
-
-    if placeSearch['candidates']:
-        places = placeSearch['candidates']
-        place_id = places[0]['place_id']
-        detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json?place_id="
-        detailsResponse = requests.get(f'{detailsUrl}{place_id}&key={API_KEY}')
-        details = detailsResponse.json()
-        result = details['result']
-        needed = {
-            'ID': result['id'],
-            'Provider': result['scope'],
-            'Name': result['name'],
-            'Location': {
-                'Lat': result['geometry']['location']['lat'],
-                'Lng': result['geometry']['location']['lng'],
-            },
-            'Address': result['formatted_address'],
-            'URI': result['url']
-        }
-        status = 'OK'
-        data = {
-            'status': status,
-            'result': needed
-        }
-        return JsonResponse(data)
+    
+    if source.lower() == 'google':
+        return sources.google(request)
+    
     data = {
-        'status': placeSearch['status'],
-        'result': []
+        'status': 'INVALID REQUEST',
+        'error': 'Other Sources are under development'
     }
-    return JsonResponse(data)  
+    return JsonResponse(data, status=400) 
+    
+
+ 
