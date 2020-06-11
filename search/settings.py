488650376f37ce52eead441596f7fc95bf3d0859
Taiwo <taiwo@Babatundes-MacBook-Pro.local>
2020-06-11 12:45:14 +0100

    append to ALLOWED_HOSTS

--- search/settings.py
+++ search/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'place-search.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'place-search.herokuapp.com', 'place-search-docker.herokuapp.com']
 
 
 # Application definition
